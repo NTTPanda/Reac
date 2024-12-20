0c5a782c5135f4d1f8fa39a8b11e5ee1b51a8249
Rohit K Gouda <136877914+NTTPanda@users.noreply.github.com>
2024-12-20 14:21:02 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,10 +2,15 @@ import React from "react";
 import "./App.css"; // Import the CSS file
 import DownAdd from "./DownAdd"; // Import DownAdd component
 import RohitPhoto from "./images/Globe.png";
-
+import Drop from "./Drop";
 
 
 function App() {
+  const dropdownOptions = [
+    { value: "option1", label: "Option 1" },
+    { value: "option2", label: "Option 2" },
+    { value: "option3", label: "Option 3" },
+  ];
   return (
     <>
       <div className="box">
@@ -16,6 +21,11 @@ function App() {
 />
       </div>
 
+
+      <Drop
+        labelText="Select an Option"
+        options={dropdownOptions}
+      />
     </>
   );
 }
